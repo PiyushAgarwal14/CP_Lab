b016088b0d2ab6dabcb5c70003af83b37a261d96
Piyush Agarwal <83577496+PiyushAgarwal55@users.noreply.github.com>
2021-07-16 21:45:34 +0530

    Update vowel_or_consonent.c

--- vowel_or_consonent.c
+++ vowel_or_consonent.c
@@ -1,22 +1,18 @@
-// C Program to Check Whether a Character is Vowel or Consonant
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    char ch;
-    int lowercase_Vowel, uppercase_Vowel;
-
-    printf("Please Enter an alphabet: \n");
-    scanf(" %c", &ch);
-
-    lowercase_Vowel = (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
-    uppercase_Vowel = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
-
-    if (lowercase_Vowel || uppercase_Vowel) {
-    	printf("\n %c is a VOWEL.", ch);
-	}
-    else {
-    	printf("\n %c is a CONSONANT.", ch);
-	}
+    char n;
+    int menu;
+    printf("Enter any alphabet:");
+    scanf("%c",&n);
+    menu =(n=='a' || n=='e' || n=='i' || n=='o' || n=='u');
+    if(menu)
+    {
+    printf("\n%c is a vovel",n);
+    }
+    else
+   {
+    printf("\n%c is consonant",n);
+    }
     return 0;
 }
